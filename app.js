7c5a6c1700a9ce0186983e8d4630175168d79596
callmefad1 <fedihajji1@gmail.com>
2026-03-08 18:48:57 +0100

    fixed package.json

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-
-app.use(express.static("public"));
-
-
-
-
-=======
 require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
@@ -23,16 +13,10 @@ app.use(express.static("public"));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', require('./src/routes/authRoutes'));
->>>>>>> 94368a329c820a2d7de5dd256e667eede011f8d1
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
-<<<<<<< HEAD
-});
-
-=======
     console.log(`API Documentation on port http://localhost:${PORT}/api-docs`);
 
 });
->>>>>>> 94368a329c820a2d7de5dd256e667eede011f8d1
